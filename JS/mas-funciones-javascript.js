bf9d968d910f660d4d45c73064abe87ca19d7d3a
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-05-26 14:02:26 +0200

    funciones

--- JS/mas-funciones-javascript.js
+++ JS/mas-funciones-javascript.js
@@ -78,3 +78,91 @@ function saludar() {
     console.log(saludo + ', ' + nombre + ' ' + apellido + '!');
 }
 saludar('Adiós', 'Carlos', 'Fernández');
+
+//Declaración global window.hola(), this.hola() y hola() 
+
+function hola() {
+    console.log('Buenas tardes');
+}
+console.log(this);
+// Buenas tardes
+window.hola();
+// Buenas tardes
+this.hola();
+// Buenas tardes
+hola();
+
+//SCOPE
+// function saludar() {
+//     var edad = 14;
+// }
+// saludar();
+// error
+//console.log(edad);
+// function saludar() {
+//     edad = 25; //Variable global tiene SCOPE global
+// }
+// function saludar2() {
+//     // 25
+//     console.log(edad);
+// }
+// console.log(edad);
+// saludar();
+// saludar2()
+// 25
+// console.log(edad);
+
+// Declarar con var al principio la convierte en GLOBAL
+// var edad = 25; //identico que "edad = 25;"
+// function saludar() {
+//     // 25
+//     console.log(edad);
+// }
+// saludar();
+
+//Con var
+function saludar1() {
+    // var i;
+    for (var i = 0; i < 10; i++) {
+        console.log('Hola');
+    }
+    // 10, aunque se podría pensar que i en este punto no debería existir porque se creó en el ámbito del for
+    console.log(i);
+}
+saludar1();
+//Con let
+function saludar2() {
+    for (let i = 0; i < 10; i++) {
+        console.log('Hola');
+    }
+    // error porque con let no hay Hoisting
+    //console.log(i);
+}
+saludar2();
+
+// error porque con let no hay Hoisting
+//console.log(a);
+let a = 4;
+function varTest1() {
+    let x = 31;
+    if (true) {
+        // error porque la variable x todavía no existe
+        //console.log(x);
+        let x = 71;
+        // 71
+        console.log(x);
+    }
+    // 31
+    console.log(x);
+}
+varTest1();
+function varTest2() {
+    let x = 31;
+    if (true) {
+        // 31
+        console.log(x);
+    }
+    // 31
+    console.log(x);
+}
+varTest2();
